4d943ebbf60c6fdda4913e81aca6e6aeafa65dfa
okanokumus <okanokumuss@gmail.com>
2017-03-28 09:17:53 +0300

    Defuzzification Methods

--- Defuzzificaiton_Methods/Defuzzification_Methods.m
+++ Defuzzificaiton_Methods/Defuzzification_Methods.m
@@ -31,7 +31,7 @@ axis([-10 10 0 1])
 title('Defuzzification Methods')
 xlabel('x')
 ylabel('membership(x)')
-
+pause
 % 1. Centroid Defuzzification
 % Centroid defuzzification returns the center of area under the curve. If
 % you think of the area as a plate of equal density, the centroid is the
@@ -39,7 +39,7 @@ ylabel('membership(x)')
 x1 = defuzz(x,mf,'centroid');
 h1 = line([x1 x1],[-0.2 1.2],'Color','g');
 t1 = text(x1,0.2,' centroid','Color','green','FontWeight','bold');
-
+pause
 % 2. Bisector Method
 % The bisector is the vertical line that will divide the region into two
 % sub-regions of equal area. It is sometimes, but not always coincident with
@@ -47,7 +47,7 @@ t1 = text(x1,0.2,' centroid','Color','green','FontWeight','bold');
 x2 = defuzz(x,mf,'bisector');
 h2 = line([x2 x2],[-0.4 1.2],'Color','b');
 t2 = text(x2,0.4,' bisector','Color','b','FontWeight','bold');
-
+pause
 % 3,4,5. Middle, Smallest, and Largest of Maximum Methods
 % MOM, SOM, and LOM stand for Middle, Smallest, and Largest of Maximum,
 % respectively. These three methods key off the maximum value assumed by the
@@ -58,14 +58,13 @@ t2 = text(x2,0.4,' bisector','Color','b','FontWeight','bold');
 x3 = defuzz(x,mf,'mom');
 x4 = defuzz(x,mf,'som');
 x5 = defuzz(x,mf,'lom');
-
 h3 = line([x3 x3],[-0.7 1.2],'Color','c');
 t3 = text(x3,0.3,' MOM','Color','c','FontWeight','bold');
 h4 = line([x4 x4],[-0.8 1.2],'Color','k');
 t4 = text(x4,0.1,' SOM','Color','k','FontWeight','bold');
 h5 = line([x5 x5],[-0.6 1.2],'Color','m');
 t5 = text(x5,0.1,' LOM','Color','m','FontWeight','bold');
-
+pause
 % Which of these methods is the right one? There's no simple answer. 
 % But if you want to get started quickly, generally the centroid method is 
 % good enough. Later you can always change your defuzzification method to 
@@ -80,6 +79,7 @@ h5.Color = gray;
 t5.Color = gray;
 h1.Color = 'red';
 t1.Color = 'red';
+pause
 %% Part-2 (mostly depends on the Part-1)
 gray = 0.7*[1 1 1];
 x = 0:0.01:12;
@@ -91,25 +91,25 @@ title('Individual MFs')
 mf = max(mf2,max(mf1,mf3));
 subplot(212);plot(x,mf);
 title('Membership Scheme')
+pause
 x1 = defuzz(x,mf,'centroid');
 h1 = line([x1 x1],[-0.2 1.2],'Color','g');
 t1 = text(x1,0.2,' centroid','Color','green','FontWeight','bold');
-
+pause
 x2 = defuzz(x,mf,'bisector');
 h2 = line([x2 x2],[-0.4 1.2],'Color','b');
 t2 = text(x2,0.4,' bisector','Color','b','FontWeight','bold');
-
+pause
 x3 = defuzz(x,mf,'mom');
 x4 = defuzz(x,mf,'som');
 x5 = defuzz(x,mf,'lom');
-
 h3 = line([x3 x3],[-0.7 1.2],'Color','c');
 t3 = text(x3,0.3,' MOM','Color','c','FontWeight','bold');
 h4 = line([x4 x4],[-0.8 1.2],'Color','k');
 t4 = text(x4,0.1,' SOM','Color','k','FontWeight','bold');
 h5 = line([x5 x5],[-0.6 1.2],'Color','m');
 t5 = text(x5,0.1,' LOM','Color','m','FontWeight','bold');
-
+pause
 
 h2.Color = gray;
 t2.Color = gray;
